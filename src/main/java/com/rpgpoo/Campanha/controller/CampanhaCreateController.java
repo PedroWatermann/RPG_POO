91f0c21a2dca0dafe99abebd3b975bf7f3e2dbcd
GuzzDoritos <gustavo.bragada@hotmail.com>
2026-06-10 19:44:46 -0300

    Cria CampanhaCreate view e controller

---
+++ src/main/java/com/rpgpoo/Campanha/controller/CampanhaCreateController.java
@@ -0,0 +1,15 @@
+package com.rpgpoo.Campanha.controller;
+
+import com.rpgpoo.Campanha.view.CampanhaCreateView;
+import com.rpgpoo.Campanha.view.CampanhaSelectView;
+import com.rpgpoo.Gerenciador.Gerenciador;
+
+public class CampanhaCreateController {
+    private final CampanhaCreateView view;
+    private final Gerenciador gerenciador;
+
+    public CampanhaCreateController(CampanhaCreateView view, Gerenciador gerenciador) {
+        this.view = view;
+        this.gerenciador = gerenciador;
+    }
+}
